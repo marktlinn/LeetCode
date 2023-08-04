bcc016c52a0484dc33f7acd0c1285579a6c0f1c9
Mark Linn <marktlinn@gmail.com>
2023-08-04 20:11:13 +0200

    add: challenge info

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,3 +1,28 @@
+/*
+Link: https://leetcode.com/problems/move-zeroes/?envType=study-plan-v2&envId=leetcode-75
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+*/
+
 import java.util.Arrays;
 
 class MoveZeroes {
